80120159a2d8d4bc4cc5c4aeb0cf9d476c0c5b12
tadaren <ptymaato@yahoo.co.jp>
2017-10-22 22:45:13 +0900

    通知システムを実装

---
+++ inform.py
@@ -0,0 +1,10 @@
+import line_api
+import setting
+from time_table import *
+
+
+table = time_table_string(get_time_table(get_date("明日")))
+task = task_string(get_task(get_date("明日")))
+event = event_string(get_event(get_date("明日")))
+
+line_api.push_message(setting.ID, '{}\n{}\n{}'.format(table, task, event))
--- setting.py
+++ setting.py
@@ -3,10 +3,11 @@ EVENT = 'db/event.csv'
 TASK = 'db/task.csv'
 CHANGE = 'db/time_table_change.csv'
 
-
 ACCESS_TOKEN = ''
 CHANNEL_SECRET = ''
 
 CERT_FILE = ''
 KEY_FILE = ''
 CA_FILE = ''
+
+ID = ''
