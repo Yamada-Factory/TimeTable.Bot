01a026b7bf8af998f3893deefef9b99cd152633d
tadaren <ptymaato@yahoo.co.jp>
2017-10-22 23:06:03 +0900

    軽微な変更

--- inform.py
+++ inform.py
@@ -7,4 +7,4 @@ table = time_table_string(get_time_table(get_date("明日")))
 task = task_string(get_task(get_date("明日")))
 event = event_string(get_event(get_date("明日")))
 
-line_api.push_message(setting.ID, '{}\n{}\n{}'.format(table, task, event))
+line_api.push_message(setting.ID, [table, task, event])
